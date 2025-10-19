12f706b754aeadad88257aea2597828e1577552c
Arun <nalabalaaruneswarreddy2601@gmail.com>
2025-10-19 14:54:34 +0530

    Comment out resource_type and public_id in cloudConfig
    Comment out resource_type and public_id parameters in CloudinaryStorage configuration.

--- cloudConfig.js
+++ cloudConfig.js
@@ -13,13 +13,13 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: 'syllabite_DEV',
-    resource_type: 'raw',       // raw for PDFs
+    // resource_type: 'raw',       // raw for PDFs
     allowed_formats: ['pdf'],   // only PDFs
-    public_id: (req, file) => {
-      // generate unique name without extension
-      const name = file.originalname.replace(/\.[^/.]+$/, '');
-      return `${name}-${Date.now()}`;  // Cloudinary will keep .pdf internally
-    }
+    // public_id: (req, file) => {
+    //   // generate unique name without extension
+    //   const name = file.originalname.replace(/\.[^/.]+$/, '');
+    //   return `${name}-${Date.now()}`;  // Cloudinary will keep .pdf internally
+    // }
 
     // use_filename: true,         // keep the original name
     // unique_filename: false,      // don’t randomize it
